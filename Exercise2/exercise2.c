44cb50ea3c56e564a97b77045ff7c9c36c539595
ARAjconrod <55189100+ARAjconrod@users.noreply.github.com>
2019-09-11 14:27:23 +0200

    Version 2

--- Exercise2/exercise2.c
+++ Exercise2/exercise2.c
@@ -2,11 +2,13 @@
 #include<stdio.h>
 void main(){
 	/* Here, you should write the source code of your program */
-	int a,b,c;
+	int a,b,c,d;
 	printf("Entry value of first number");
 	scanf("%d",&a);
 	printf("entry value of second number");
 	scanf("%d",&b);
-	c=a+b;
-	printf("The result of the sum is %d",c);
+	printf("entry value of third number");
+	scanf("%d",&c);
+	d=a+b+c;
+	printf("The result of the sum is %d",d);
 }
